d2d0d4cc4d4a93c94f250ce76491a4c8d97f6023
Valmir Jr <valmir.junior@ee.ufcg.edu.br>
2019-12-23 03:28:20 -0300

    update: application registration

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -33,4 +33,26 @@ export function* signIn({ payload }) {
   }
 }
 
-export default all([takeLatest("@auth/SIGN_IN_REQUEST", signIn)]);
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, "users", {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+
+    history.push("/");
+  } catch (err) {
+    toast.error("Registration failed, please verify your information");
+
+    yield put(signFailure());
+  }
+}
+
+export default all([
+  takeLatest("@auth/SIGN_IN_REQUEST", signIn),
+  takeLatest("@auth/SIGN_UP_REQUEST", signUp),
+]);
